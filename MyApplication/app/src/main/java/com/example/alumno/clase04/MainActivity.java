39c5da30d65870a7da6122a3e987768369e0d6c2
alumno <alumno>
2017-04-20 22:09:24 -0300

    2

--- MyApplication/app/src/main/java/com/example/alumno/clase04/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/alumno/clase04/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.alumno.clase04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this.getActionBar().se
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.nuevo_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Log.d("Click", "Opcion de menu");
+
+        return super.onOptionsItemSelected(item);
     }
 }
